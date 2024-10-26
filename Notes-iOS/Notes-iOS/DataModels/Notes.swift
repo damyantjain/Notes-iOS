8c6665d68db4be63ff9593b99e882b3c2a188190
damyantjain <damyant9999@gmail.com>
2024-10-26 09:26:57 -0400

    Implemented langing page

---
+++ Notes-iOS/Notes-iOS/DataModels/Notes.swift
@@ -0,0 +1,12 @@
+//
+//  Notes.swift
+//  Notes-iOS
+//
+//  Created by Damyant Jain on 10/26/24.
+//
+
+struct Notes: Codable {
+    let _id: String
+    let userid: String
+    let text: String
+}
--- Notes-iOS/Notes-iOS/ViewControllers/LandingViewController.swift
+++ Notes-iOS/Notes-iOS/ViewControllers/LandingViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class LandingViewController: UIViewController {
-    
+
     let landingView = LandingView()
-    
+    var notesList = [Notes]()
     override func loadView() {
         view = landingView
     }
@@ -18,6 +18,23 @@ class LandingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add, target: self,
+            action: #selector(onAddBarButtonTapped))
+        landingView.notesTableView.delegate = self
+        landingView.notesTableView.dataSource = self
+        landingView.notesTableView.separatorStyle = .none
+        
+        notesList.append(Notes(_id: "3426236256", userid: "damyantjain", text: "This is my first note"))
+        notesList.append(Notes(_id: "723645234", userid: "damyant", text: "This is my second note"))
+    }
+    
+    @objc func onAddBarButtonTapped() {
+        
+    }
+    
+    func deleteSelectedFor(noteIndex: Int){
+        
     }
 
 }
