8d7d9dc1d99004605e65712098a7b5b5f2d7e77c
damyantjain <damyant9999@gmail.com>
2024-10-28 23:11:09 -0400

    Added Note Display files

---
+++ Notes-iOS/Notes-iOS/Views/NoteDisplay.swift
@@ -0,0 +1,20 @@
+//
+//  NoteDisplay.swift
+//  Notes-iOS
+//
+//  Created by Damyant Jain on 10/28/24.
+//
+
+import UIKit
+
+class NoteDisplay: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
