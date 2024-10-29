b8a99bb11f3c929e96fa6a46d9725ae270f92911
damyantjain <damyant9999@gmail.com>
2024-10-28 23:11:25 -0400

    Implemented Notes Display View

--- Notes-iOS/Notes-iOS/ViewControllers/NoteDisplayViewController.swift
+++ Notes-iOS/Notes-iOS/ViewControllers/NoteDisplayViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class NoteDisplayViewController: UIViewController {
 
+    let noteDisplayView = NoteDisplay()
+    var note: Note?
+
+    override func loadView() {
+        view = noteDisplayView
+        title = "Note"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpNote()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpNote() {
+        if let noteText = note {
+            noteDisplayView.noteBody.text = noteText.text
+        }
     }
-    */
-
 }
--- Notes-iOS/Notes-iOS/Views/NoteDisplay.swift
+++ Notes-iOS/Notes-iOS/Views/NoteDisplay.swift
@@ -9,12 +9,58 @@ import UIKit
 
 class NoteDisplay: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var scrollView: UIScrollView!
+    var noteBody: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        setUpScrollView()
+        setUpNoteBody()
+
+        initConstraints()
     }
-    */
+
+    func setUpScrollView() {
+        scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(scrollView)
+    }
+
+    func setUpNoteBody() {
+        noteBody = UILabel()
+        noteBody.textColor = .black
+        noteBody.font = UIFont(name: "AvenirNext-Medium", size: 16)
+        noteBody.numberOfLines = 0
+        noteBody.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(noteBody)
+    }
+
+    func initConstraints() {
+        NSLayoutConstraint.activate([
+            
+            //scroll view
+            scrollView.topAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            
+            //note body
+            noteBody.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
+            noteBody.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            noteBody.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            noteBody.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
+            noteBody.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32),
+
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
